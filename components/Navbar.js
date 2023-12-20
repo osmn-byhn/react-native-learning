19e59aaa0447ba8b2a267ab8f27ef12b3b4541c6
osmn-byhn <osmanbeyhan12@gmail.com>
2023-12-21 01:08:01 +0300

    first commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Navbar from './components/Navbar';
 
-export default function App() {
+export default function Navbar() {
   return (
     <View style={styles.container}>
-      <Navbar />
-      <Text>Kral ilk uygulama amk...</Text>
+      <Text>Header</Text>
       <StatusBar style="auto" />
     </View>
   );
